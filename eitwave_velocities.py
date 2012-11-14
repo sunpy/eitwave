2ec01ac8fb1c35fc0206c526638db6d5e06d18db
aringlis <a.r.inglis@gmail.com>
2012-11-14 17:24:55 -0500

    developmental update: added into eitwaveutils a function to strip out wavefront position and width information. Added a function to eitwave_velocities.py which performs a linear fit to the position measurements of a given column.

--- eitwave_velocities.py
+++ eitwave_velocities.py
@@ -16,4 +16,27 @@ def velocity_histogram(velocity):
         else:
             print('No velocity data for frame ' + str(i) + '. Skipping.')
 
+def velocity_polyfit(velocity, column):
+    #wrong! Want to input positions, not velocities.
+    vel=[]
+    for i in range(0,len(velocity)):
+        if velocity[i] == []:
+            vel.append(0)
+        else:
+            if velocity[i][column] == []:
+                vel.append(0)
+            else:
+                vel.append(velocity[i][column])
+
+    x=np.linspace(1,19,19)
+    w=np.polyfit(x,vel,1)
+    p=np.poly1d(w)
+
+    plt.title('Velocity linear fit')
+    plt.xlabel('Frame')
+    plt.ylabel('Position (deg)')
+    plt.plot(x,vel,'g.')
+    plt.plot(x,p(x),'r-')
+    plt.show()
+
     

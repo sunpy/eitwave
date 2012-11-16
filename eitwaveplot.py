ec71725c01eb3950a4c2cfb47ea43a9c1d49b97c
aringlis <a.r.inglis@gmail.com>
2012-11-15 19:18:25 -0500

    bug fix for eitwaveplot.py. Now import numpy as np

--- eitwaveplot.py
+++ eitwaveplot.py
@@ -1,15 +1,16 @@
 import scipy
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 def velocity_histogram(velocity):
+    """Plot a histogram of the wavefront velocity values for each eit wave image frame"""
     for i in range(0,len(velocity)):
         if velocity[i] != []:
             print('Plotting velocity histogram for frame ' + str(i))
             vel=filter(None,velocity[i])
             plt.hist(vel,range=[-40,10],bins=50)
             plt.title('Velocity Histogram for frame ' + str(i))
-            plt.xlabel('Velocity deg/frame')
+            plt.xlabel('Velocity (deg/frame)')
             plt.ylabel('N')
             plt.show()
             plt.pause(0.5)
@@ -17,6 +18,7 @@ def velocity_histogram(velocity):
             print('No velocity data for frame ' + str(i) + '. Skipping.')
 
 def width_histogram(width):
+    """Plot a histogram of the wavefront width values for each eit wave image frame"""
     for i in range(0,len(width)):
         if width[i] != []:
             print('Plotting width histogram for frame ' + str(i))
@@ -32,7 +34,7 @@ def width_histogram(width):
     
 
 def velocity_polyfit(position, column):
-    #wrong! Want to input positions, not velocities.
+    """Perform a polynomial fit to the wavefront position as a function of time, for a given image column"""
     pos=[]
     for i in range(0,len(position)):
         if position[i] == []:
@@ -53,10 +55,14 @@ def velocity_polyfit(position, column):
     #keep everything that's not NaN
     pos=pos[u]
     x=x[u]
-    
+
+    #perform a polynomial fit to the data (pos). x is the independent variable,
+    #and the third parameter is the order of the polynomial.
     w=np.polyfit(x,pos,1)
+    #designate p as the polynomial function
     p=np.poly1d(w)
 
+    #plot the polynomial fit over the original position data
     plt.title('Velocity linear fit')
     plt.xlabel('Frame')
     plt.ylabel('Position (deg)')

810dc5049c327afbfaf95428611b701e6cfa60da
aringlis <a.r.inglis@gmail.com>
2013-05-08 13:42:58 -0400

    now include the model of the detected wave as an output, transformed back into heliocentric coordinates. Transform parameters are now also saved as an output.

--- data_hough_detect.py
+++ data_hough_detect.py
@@ -54,7 +54,7 @@ def main(source_data='.jp2',
     # Flares!
     print('Number of flares found = ' + str(len(hek_result)))
 
-    for flare in hek_result[10:11]:
+    for flare in hek_result[12:13]:
 
         if feed_directory is None:
             print('Acquiring data for flare')
@@ -181,6 +181,9 @@ def main(source_data='.jp2',
         #detection
         wavefront = eitwaveutils.fit_wavefront(posdiffs, detection)
 
+        #transform the detected model wavefront back into heliocentric coordinates so it can be overlayed
+        wavefront_hc = eitwaveutils.map_reravel(wavefront[1],params,verbose=True)
+
         #strip out the velocity information from the wavefront fitting
         velocity = eitwaveutils.wavefront_velocity(wavefront[0])
 
@@ -189,7 +192,7 @@ def main(source_data='.jp2',
 
         visualize(detection)
 
-    return maps, new_maps, diffs, threshold_maps, binary_maps, detection, wavefront, velocity, pos_width, persistence_maps
+    return maps, new_maps, diffs, threshold_maps, binary_maps, detection, wavefront, velocity, pos_width, persistence_maps, wavefront_hc,params
 
 
 if __name__ == '__main__':

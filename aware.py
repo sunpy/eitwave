6b5cfdefcb27990df75ce326c8e4172862cd4b41
aringlis <a.r.inglis@gmail.com>
2014-02-26 14:27:56 -0500

    renamed files for clarity.

--- aware.py
+++ aware.py
@@ -1,6 +1,6 @@
 
 from visualize import visualize
-import eitwaveutils
+import aware_utils
 import copy
 import os
 import cPickle as pickle
@@ -13,7 +13,7 @@ import os
 def main(source_data='.jp2',
          time_range=TimeRange('2011/10/01 09:45:00', '2011/10/01 10:15:59'),
          algorithm='hough', feed_directory='~/Data/eitwave/jp2/20111001_jp2/',
-         use_pickle='maps.pkl',diff_type='running'):
+         use_pickle=None,diff_type='running'):
     '''
     source_data { jp2 | fits | test }
     look for helioviewer JP2 files, FITS files, or load the test data
@@ -58,12 +58,12 @@ def main(source_data='.jp2',
 
         if feed_directory is None:
             print('Acquiring data for flare')
-            filelist = eitwaveutils.acquire_data(data_storage, source_data,
+            filelist = aware_utils.acquire_data(data_storage, source_data,
                                                  flare)
         else:
             # Assumes that the necessary files are already present
 
-            filelist = eitwaveutils.listdir_fullpath(feed_directory,
+            filelist = aware_utils.listdir_fullpath(feed_directory,
                                                      filetype ='jp2')
 
         #filter to only grab the data files with the source_data extn in the directory
@@ -78,7 +78,7 @@ def main(source_data='.jp2',
         files = []
         for f in files_tmp:
             fhv = f.split(os.sep)[-1]
-            if eitwaveutils.hv_filename2datetime(fhv) > \
+            if aware_utils.hv_filename2datetime(fhv) > \
             parse_time(flare['event_starttime']):
                 files.append(f)
         print('Number of files :' + str(len(files)))
@@ -87,8 +87,7 @@ def main(source_data='.jp2',
             return None
 
         # Define the transform parameters
-        # params = eitwaveutils.params(flare='test')
-        params = eitwaveutils.params(flare)
+        params = aware_utils.params(flare)
 
         # read in files and accumulate them
         if use_pickle != None:
@@ -100,7 +99,7 @@ def main(source_data='.jp2',
             diffs = a[2]
             pfile.close()
         else:
-            maps = eitwaveutils.accumulate(files[6:30], accum=1, nsuper=4,
+            maps = aware_utils.accumulate(files[6:30], accum=1, nsuper=4,
                                    verbose=True)
 
             #temporary fix for exposure control and S/N changes
@@ -111,49 +110,45 @@ def main(source_data='.jp2',
             maps=long_maps
 
             # Unravel the maps
-            new_maps = eitwaveutils.map_unravel(maps, params, verbose=True)
+            new_maps = aware_utils.map_unravel(maps, params, verbose=True)
             #return new_maps
 
             #sometimes unravelling maps leads to slight variations in the unraveled
             #image dimensions.  check dimensions of maps and resample to dimensions
             #of first image in sequence if need be.
             #new_maps[0].peek()
-            new_maps = eitwaveutils.check_dims(new_maps)
+            new_maps = aware_utils.check_dims(new_maps)
 
             # calculate the differences
             if diff_type == 'base':
-                diffs=eitwaveutils.map_basediff(new_maps)
+                diffs=aware_utils.map_basediff(new_maps)
             else:
-                diffs = eitwaveutils.map_diff(new_maps)
+                diffs = aware_utils.map_diff(new_maps)
 
-            # save the outpout
-            #output = open(feed_directory + 'maps.pkl', 'wb')
-            ##pickle.dump([maps, new_maps, diffs], output, protocol=0)
-            #output.close()
 
-        #generate persistence maps
+        #generate persistence maps - currently bugged, so skip this step
         #persistence_maps = eitwaveutils.map_persistence(diffs)
         persistence_maps=[]
 
         #determine the threshold to apply to the difference maps.
         #diffs > diff_thresh will be True, otherwise False.
-        threshold_maps = eitwaveutils.map_threshold(new_maps, factor=0.2)
+        threshold_maps = aware_utils.map_threshold(new_maps, factor=0.2)
         #return threshold_maps
 
         # transform difference maps into binary maps
-        binary_maps = eitwaveutils.map_binary(diffs, threshold_maps)
+        binary_maps = aware_utils.map_binary(diffs, threshold_maps)
 
         if algorithm == 'hough':
             # detection based on the hough transform
-            detection = eitwaveutils.hough_detect(binary_maps, vote_thresh=10)
+            detection = aware_utils.hough_detect(binary_maps, vote_thresh=10)
         elif algorithm == 'prob_hough':
             # detection based on the probabilistic hough transform.  Takes the
             # keywords of the probabilistic hough transform - see the documentation
             # of skimage.transform.probabilistic_hough (scikit-image.org)
-            detection = eitwaveutils.prob_hough_detect(binary_maps, threshold=10)
+            detection = aware_utils.prob_hough_detect(binary_maps, threshold=10)
 
         # Remove areas that are too small or that don't have enough detections
-        detection = eitwaveutils.cleanup(detection,
+        detection = aware_utils.cleanup(detection,
                                          size_thresh=50,
                                          inv_thresh=8)
 
@@ -164,7 +159,7 @@ def main(source_data='.jp2',
         #diffmaps in the region defined by 'detection'. Simplest case: fit a
         #Gaussian in the y-direction for some x or range of x.
         #eitwaveutils.fit_wavefront should probably take the arguments of fitfunc.
-        #use 'detection' to guess starting fit parameters?
+        #use 'detection' to guess starting fit parameters.
 
         #get just the positive elements of the difference map. Perform fitting on
         #these positive diffmaps.
@@ -175,16 +170,16 @@ def main(source_data='.jp2',
 
         #fit a function to the difference maps in the cases where there has been a
         #detection
-        wavefront = eitwaveutils.fit_wavefront(posdiffs, detection)
+        wavefront = aware_utils.fit_wavefront(posdiffs, detection)
 
         #transform the detected model wavefront back into heliocentric coordinates so it can be overlayed
-        wavefront_hc = eitwaveutils.map_reravel(wavefront[1],params,verbose=True)
+        wavefront_hc = aware_utils.map_reravel(wavefront[1],params,verbose=True)
 
         #strip out the velocity information from the wavefront fitting
-        velocity = eitwaveutils.wavefront_velocity(wavefront[0])
+        velocity = aware_utils.wavefront_velocity(wavefront[0])
 
         #strip out the position and width information from the wavefront fitting
-        pos_width = eitwaveutils.wavefront_position_and_width(wavefront[0])
+        pos_width = aware_utils.wavefront_position_and_width(wavefront[0])
 
         visualize(detection_maps)
 
